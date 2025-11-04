858393eeafd5aae7573b35d6c5bf118f94d04a63
Artem Nizamiiev <upuyayua@gmail.com>
2025-11-04 23:11:54 +0200

    Solved3

--- task.sql
+++ task.sql
@@ -6,7 +6,8 @@ CREATE PROCEDURE get_warehouse_product_inventory(IN WarehouseID INT)
 BEGIN
     SELECT Products.Name, ProductInventory.WarehouseAmount
     FROM Products
-    JOIN ProductInventory ON ProductInventory.WarehouseID = WarehouseID
+    JOIN ProductInventory ON Products.ID = ProductInventory.ProductID
+    WHERE ProductInventory.WarehouseID = WarehouseID;
 END //
 
 DELIMITER ;
